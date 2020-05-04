6a5c741697e245d77c48b6e6102c6419b474b309
Ramzek <40779506+Ramzek@users.noreply.github.com>
2020-05-03 20:18:05 -0400

    Creacion Clase personaje, atributos y constructor vacio

---
+++ Personaje.py
@@ -0,0 +1,18 @@
+import random as rd;
+import sys
+import Ciudad 
+import Ocupacion
+
+class Personaje:
+
+    def __init__(self):
+        self.nombre = []
+        self.edad 
+        self.ciudadOrigen
+        self.Ocupacion
+        self.personalidad 
+
+
+
+
+    
