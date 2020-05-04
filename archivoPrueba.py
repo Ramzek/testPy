57b639a4735a89fe6c20bc457628e83d064283af
Ramzek <40779506+Ramzek@users.noreply.github.com>
2020-05-03 23:36:54 -0400

    feat: Pruebas tag

--- archivoPrueba.py
+++ archivoPrueba.py
@@ -1,6 +1,6 @@
 print("Este es un archivo de prueba")
 
-print("A continuacion cambios comparacion entre rama-A y rama-B")
+print("Pruebas Git, tags")
 
 
-print("Esto es un cambio de la rama-B y la rama-A
+
